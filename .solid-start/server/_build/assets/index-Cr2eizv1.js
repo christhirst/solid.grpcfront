c8a2ba313f3281a77de0bcc157ca9cb7c5d0ccbf
christhirst <chrisgluon@gmail.com>
2026-09-20 08:40:16 +0200

    rework

--- .solid-start/server/_build/assets/index-Cr2eizv1.js
+++
@@ -1,9 +0,0 @@
-import { ssr, ssrHydrationKey } from "solid-js/web";
-var _tmpl$ = ["<main", ' class="relative min-h-screen"><div class="mesh-gradient"></div><div class="grain-overlay"></div><div class="relative z-10 mx-auto max-w-7xl 2xl:max-w-[90rem] px-4 sm:px-6 py-24 sm:py-32 text-center"><h1 class="text-5xl font-extrabold tracking-tight sm:text-6xl md:text-7xl">Solid<span class="gradient-text">Flow</span></h1><p class="mt-6 text-lg text-[#8b8b9e]">Your workspace is ready.</p></div></main>'];
-function Home() {
-  return ssr(_tmpl$, ssrHydrationKey());
-}
-export {
-  Home as default
-};
-//# sourceMappingURL=index-Cr2eizv1.js.map
--- .solid-start/server/_build/assets/live-DeiOVy5U.js
+++
@@ -1,7 +0,0 @@
-async function POST(event) {
-  return GET(event);
-}
-export {
-  POST
-};
-//# sourceMappingURL=live-DeiOVy5U.js.map
